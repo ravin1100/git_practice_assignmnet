fa0e0f64a6f550eaa9b260c03889ac7bfc99bef9
Ravin Kumar Jangir <ravinjangir.rj1100@gmail.com>
2023-01-08 21:28:52 +0530

    Prime Number & Palindrome problem done

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+let str="masai";
+let bag="";
+
+for(let i=str.length-1; i>=0; i--){
+   bag=bag+str[i];
+}
+if(bag=str){
+  console.log("String is a Palindrome");
+}
+else{
+  console.log("String is not a Palindrome");
+}
+
+
+  
